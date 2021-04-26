8fba65e7860ea2c004620a9408bf96c34dbbb33b
saba1101 <55694011+saba1101@users.noreply.github.com>
2021-04-26 17:02:49 +0400

    Add files via upload

--- script/animation.js
+++ script/animation.js
@@ -1,5 +1,4 @@
 
-
 // $('.main-title').on('mouseover',function(e){
 //     var rect = e.target.getBoundingClientRect();
 //     var x = e.clientX - rect.left; 
@@ -61,4 +60,60 @@
         else{
             console.log("%cTILT JS is active", "color:green; font-size: 13px; font-weight:700 ");
         }
+    })
+
+
+
+    function images() {
+        var bgComponents = [
+            "lime_cube",
+            "lime_cube_fragmented",
+            "lime_cube_small_fragmented",
+            "purple_cube_def",
+            "purple_cube_fragmented",
+            "red_cube",
+            "red_cube_def",
+            "red_cube_fragmented",
+            "red_cube_small_fragmented",
+            "lime_cube",
+            "lime_cube_fragmented",
+            "lime_cube_small_fragmented",
+            "purple_cube_def",
+            "purple_cube_fragmented",
+            "red_cube",
+            "red_cube_def",
+            "red_cube_fragmented",
+            "red_cube_small_fragmented",
+            "lime_cube",
+            "lime_cube_fragmented",
+        ]
+
+
+        bgComponents.forEach(el => {
+            $('.left, .right', '.bg-component-wrapper').prepend(`<img class="bg-components" src="../assets/images/background/${el}.png">`) 
+            let Maxwidth = window.innerWidth;
+            let Maxheight = window.innerHeight;
+            Maxwidth -= 30;
+            Maxheight -= 20;
+            Maxwidth = (Maxwidth < 0) ? Maxwidth *= -1 : Maxwidth
+            Maxheight = (Maxheight < 0) ? Maxheight *= -1 : Maxheight
+            $('.bg-components').each((ind, el)=> {
+                $(el).css({
+                    left: `${Math.ceil(Math.random() * Maxwidth)}px`,
+                    top:  `${Math.ceil(Math.random() * Maxheight)}px`
+                })
+            })
+
+        });
+    
+    }
+
+    images()
+
+
+
+    $('.bg-components').hide(0)
+
+    $(window).on("load",function(){
+        $('.bg-components').show(1000)
     })
